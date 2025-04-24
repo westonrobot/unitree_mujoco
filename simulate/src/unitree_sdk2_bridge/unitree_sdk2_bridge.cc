951d5b3fd724625b8c578a00f35cf54ae3d60a0a
Ruixiang Du <ruixiang.du@gmail.com>
2025-04-24 23:09:15 +0800

    hg: fixed missing low state crc value

--- simulate/src/unitree_sdk2_bridge/unitree_sdk2_bridge.cc
+++ simulate/src/unitree_sdk2_bridge/unitree_sdk2_bridge.cc
@@ -1,5 +1,29 @@
 #include "unitree_sdk2_bridge.h"
 
+namespace {
+inline uint32_t Crc32Core(uint32_t *ptr, uint32_t len) {
+    uint32_t xbit = 0;
+    uint32_t data = 0;
+    uint32_t CRC32 = 0xFFFFFFFF;
+    const uint32_t dwPolynomial = 0x04c11db7;
+    for (uint32_t i = 0; i < len; i++) {
+        xbit = 1 << 31;
+        data = ptr[i];
+        for (uint32_t bits = 0; bits < 32; bits++) {
+            if (CRC32 & 0x80000000) {
+                CRC32 <<= 1;
+                CRC32 ^= dwPolynomial;
+            } else
+                CRC32 <<= 1;
+            if (data & xbit) CRC32 ^= dwPolynomial;
+
+            xbit >>= 1;
+        }
+    }
+    return CRC32;
+};
+}
+
 UnitreeSdk2Bridge::UnitreeSdk2Bridge(mjModel *model, mjData *data) : mj_model_(model), mj_data_(data)
 {
     CheckSensor();
@@ -163,6 +187,8 @@ void UnitreeSdk2Bridge::PublishLowStateHg()
             GetWirelessRemote();
             memcpy(&low_state_hg_.wireless_remote()[0], &wireless_remote_, 40);
         }
+
+        low_state_hg_.crc() = Crc32Core((uint32_t *)&low_state_hg_, (sizeof(unitree_hg::msg::dds_::LowState_) >> 2) - 1);
 
         low_state_hg_puber_->Write(low_state_hg_);
     }
